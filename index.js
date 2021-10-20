3d169ef843673d3eeb4bbbc2a5e9f52f05dfd0a5
PaulNguyen <paulnguyen2207@gmail.com>
2021-10-20 23:33:44 +1100

    Completed Assigment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution here
+     
+function findMatching(drivers,name) {
+    return drivers.filter( function (value){
+      return value.toLowerCase() === name.toLowerCase()
+    })
+  }
+    console.log(findMatching('Susan'));
+
+function fuzzyMatch (drivers,letter){
+      return drivers.filter (function(value){
+          return value.toLowerCase().indexOf(letter.toLowerCase())=== 0})
+  
+      }
+
+function matchName(drivers, matchName) {
+  return drivers.filter (function(value){
+    value.name.toLowerCase()===matchName.toLowerCase()
+  })
+}
+
+function matchName(drivers,matchName) {
+  return drivers.filter (function(value){
+    return value.name.toLowerCase()===matchName.toLowerCase()
+  })
+}
+
+console.log(matchName("BoBby"));
